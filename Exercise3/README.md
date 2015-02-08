7ce9725fe3d95f17f8d32f0053f6be62707b254e
jkeifer <jarrettkeifer@gmail.com>
2015-02-08 13:08:59 -0800

    Exercise 3

---
+++ Exercise3/README.md
@@ -0,0 +1,53 @@
+Exercise 3: Vector Operations in OGR
+====================================
+for GEOG 410/510 winter term 2015
+
+
+**Instructions**
+
+The purpose of this exercise is to provide an example of
+a script that uses the OGR python bindings to preform a
+basic vector analysis operation. Given the complexity of even
+this simple operation, unlike previous exercises, this
+exercise is provided with an editable .py text file.
+Moreover, the code has appropriate function and variable names.
+All that is missing is the script description, the script instructions,
+all the function docstrings, and all the comments.
+
+Thus, your task is to add all this missing documentation.
+Everywhere you find a `#` comment sign or the `""" """` docstring
+notation you must add a comment
+explaining what happens in the following statement and why (or, in the case
+of docstrings, what a function does, its arguments, and its output).
+
+I chose to provide the code for this exercise in an editable format,
+simply due to the length of the script. However, I strongly suggest
+you type out the example by hand into your own .py file rather
+than simply editing the file I've provided.
+I won't know the difference, but you will benefit;
+typing out code examples and snippets by hand is much better for
+memorization and learning than simply copying and pasting bits
+and pieces from here and there.
+
+Also included is a data directory containing a point shapefile.
+You can run the script to see what the input
+and output look like to ascertain the code's specific operations.
+When calling the script, you will need to provide the path to
+the data folder (not the shapefile) as the first argument,
+and the buffer distance (in feet) as the second argument.
+
+
+**Submissions and Points**
+
+All that need be submitted for this exercise is a .py file with the
+commented and code. Please email it to the instructor,
+attaching the file named as `LastnameFirstname_Exercise1.py`, with
+your last name and first name.
+
+This exercise is worth a total of 10 points. Points will be given
+based on the quality/thoroughness of comments.
+Keep in mind the point of this assignment is to test your code
+comprehension; do not necessarily write comments as you might
+for an actual script, but to show your understanding of every
+step of the example program's execution. You will not be penalized
+for too many comments, or for comments that are too long.
---
+++ Exercise3/exercise3.py
@@ -0,0 +1,207 @@
+# **********************************************************************
+#
+# NAME: Your Name
+# DATE: date
+# CLASS: GEOG410
+# ASSIGNMENT: Lab #
+#
+# DESCRIPTION: describe the script, what it does, how it does it,
+#       and any other important information
+#
+# INSTRUCTIONS: usage instructions, i.e., inputs, outputs, and how to
+#       run the script
+#
+# SOURCE(S): list any sources used to complete this script
+#
+# **********************************************************************
+
+# ********** IMPORT STATEMENTS **********
+import sys
+import os
+import datetime
+import argparse
+from osgeo import ogr, osr
+
+ogr.UseExceptions
+
+
+# ********** GLOBAL CONSTANTS **********
+
+POINTS = "points"
+REQUIRED = [POINTS]
+
+OUTPUT = "buffer"
+OUT_CRS = 2913
+
+
+# ********** FUNCTIONS **********
+
+def parse_args(argv):
+    """
+    """
+    a = argparse.ArgumentParser()
+    a.add_argument("workspace", type=str)
+    a.add_argument("buffer_dist", type=int)
+    b = a.parse_args(argv)
+    return b.workspace, b.buffer_dist
+
+
+def open_shapefile(path, writable=False):
+    """
+    """
+    #
+    driver = ogr.GetDriverByName('ESRI Shapefile')
+    #
+    data = driver.Open(path, writable)
+
+    #
+    if not data:
+        raise Exception ("Failed to open shapefile or workspace.")
+
+    return data
+
+
+def create_spatial_ref_from_EPSG(epsg_code):
+    """
+    """
+    #
+    spatial_ref = osr.SpatialReference()
+    #
+    spatial_ref.ImportFromEPSG(epsg_code)
+    return spatial_ref
+
+
+def check_for_and_delete_existing_layer(open_workspace, existing_layer_name):
+    """
+    """
+    #
+    for index in xrange(open_workspace.GetLayerCount()):
+        #
+        layer = open_workspace.GetLayer(index)
+        #
+        layer_name = layer.GetName()
+        #
+        if layer_name == existing_layer_name:
+            #
+            open_workspace.DeleteLayer(index)
+            return True
+    return False
+
+
+def add_spatial_reference_to_layer(workspace_path, layer_name, spatial_ref):
+    """
+    """
+    #
+    with open(os.path.join(workspace_path, layer_name) + ".prj", 'w') as outprj:
+        #
+        outprj.write(spatial_ref.ExportToWkt())
+
+
+def create_new_layer(open_workspace, out_layer_name, geometry_type,
+                     spatial_ref=None):
+    """
+    """
+    #
+    check_for_and_delete_existing_layer(open_workspace, out_layer_name)
+    #
+    out_layer = open_workspace.CreateLayer(out_layer_name,
+                                           geom_type=geometry_type)
+
+    #
+    if not out_layer:
+        raise Exception("Failed to create new layer in workspace.")
+
+    return out_layer
+
+
+def copy_fields(in_layer, out_layer):
+    """
+    """
+    #
+    in_layer_defn = in_layer.GetLayerDefn()
+    #
+    for index in xrange(0, in_layer_defn.GetFieldCount()):
+        #
+        field_defn = in_layer_defn.GetFieldDefn(index)
+        #
+        out_layer.CreateField(field_defn)
+
+
+def buffer_features(in_layer, out_poly_layer, buffer_distance,
+                    spatial_ref=None):
+    #
+    out_layer_defn = out_poly_layer.GetLayerDefn()
+
+    #
+    in_feature = in_layer.GetNextFeature()
+    while in_feature:
+        #
+        geometry = in_feature.GetGeometryRef()
+
+        #
+        if spatial_ref:
+            geometry.TransformTo(spatial_ref)
+
+        #
+        geometry = geometry.Buffer(buffer_distance)
+        #
+        out_feature = ogr.Feature(out_layer_defn)
+        #
+        out_feature.SetGeometry(geometry)
+        #
+        for index in xrange(0, out_layer_defn.GetFieldCount()):
+            #
+            out_feature.SetField(out_layer_defn.GetFieldDefn(index).GetNameRef(),
+                                 in_feature.GetField(index))
+
+        #
+        out_poly_layer.CreateFeature(out_feature)
+
+        #
+        out_feature = None
+        #
+        in_feature = in_layer.GetNextFeature()
+
+
+# ********** MAIN **********
+
+def main(argv):
+    #
+    workspace, buffer_dist = parse_args(argv)
+
+    #
+    opened_workspace = open_shapefile(workspace, True)
+
+    #
+    out_spatial_ref = create_spatial_ref_from_EPSG(OUT_CRS)
+
+    #
+    in_layer = opened_workspace.GetLayerByName(POINTS)
+
+    #
+    out_layer = create_new_layer(opened_workspace, OUTPUT, ogr.wkbPolygon)
+
+    #
+    add_spatial_reference_to_layer(workspace,
+                                   out_layer.GetName(),
+                                   out_spatial_ref)
+
+    #
+    copy_fields(in_layer, out_layer)
+
+    #
+    buffer_features(in_layer, out_layer, buffer_dist,
+                    spatial_ref=out_spatial_ref)
+
+    #
+    in_layer = None
+    out_layer = None
+    opened_workspace = None
+
+    return 0
+
+
+# ********** MAIN CHECK **********
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
